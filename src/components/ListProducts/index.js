16225681fa36a4dc4afcdb747f381a54bc0cf5be
AlderFurtado <aldermarcel26@outlook.com>
2021-02-10 00:19:41 -0300

    refactoring break component list porduct

--- src/components/ListProducts/index.js
+++ src/components/ListProducts/index.js
@@ -5,6 +5,26 @@ import { addProductInCart } from "../../store/Product/Product.actions";
 
 import styles from "./style.module.css";
 
+const itemProduct = (product, index, dispatch) => {
+  return (
+    <div key={index} className={styles.container_item}>
+      <img src={product.img} alt={product.name} />
+
+      <div>
+        <p className={styles.name}>{product.name}</p>
+        <p className={styles.description}>{product.description}</p>
+      </div>
+      <button
+        onClick={() => {
+          dispatch(addProductInCart(product));
+        }}
+      >
+        Comprar
+      </button>
+    </div>
+  );
+};
+
 function ListProducts() {
   const dispatch = useDispatch();
   const listProducts = useSelector((state) => state.product.listProducts);
@@ -13,23 +33,7 @@ function ListProducts() {
     <>
       <div className={styles.container}>
         {listProducts.map((product, index) => {
-          return (
-            <div key={index} className={styles.container_item}>
-              <img src={product.img} alt={product.name} />
-
-              <div>
-                <p className={styles.name}>{product.name}</p>
-                <p className={styles.description}>{product.description}</p>
-              </div>
-              <button
-                onClick={() => {
-                  dispatch(addProductInCart(product));
-                }}
-              >
-                Comprar
-              </button>
-            </div>
-          );
+          return itemProduct(product, index, dispatch);
         })}
       </div>
     </>
